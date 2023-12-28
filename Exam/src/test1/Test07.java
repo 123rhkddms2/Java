e42efa8561a1e1d8853363d2c6f7440b4bd63bf2
123rhkddms2 <123rhkddms2@gmail.com>
2023-12-28 15:52:21 +0900

    exam파일 업로드

---
+++ Exam/src/test1/Test07.java
@@ -0,0 +1,20 @@
+package test1;
+
+/*
+ * 날짜 : 2023/12/28
+ * 이름 : 김광은
+ * 내용 : 자바 삼항연산자 연습문제
+ */
+public class Test07 {
+	public static void main(String[] args) {
+	
+		int total = 0;
+		
+		for(int k=1; k<=10; k++) {
+			if(k%2==0 || k%3==0) {
+				total += k;
+			}
+		}
+		System.out.println("1 ~ 10까지 2와 3배수의 정수의 합 : " + total);
+	}
+}
