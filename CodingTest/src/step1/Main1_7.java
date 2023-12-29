e0d29cf46d61287f67918218515b3956ed4a0e9d
123rhkddms2 <123rhkddms2@gmail.com>
2023-12-29 15:47:56 +0900

    codingtest추가

--- CodingTest/src/step1/Main1_7.java
+++ CodingTest/src/step1/Main1_7.java
@@ -1,14 +1,12 @@
 package step1;
-
 import java.util.Scanner;
 
-public class Main1_2 {
+public class Main1_7 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		String a = sc.next();
 		
-		System.out.println(a + b);
+		System.out.println(a + "??!");
 	}
 }
