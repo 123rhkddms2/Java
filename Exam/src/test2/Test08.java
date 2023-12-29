d047fc9131ab8f036b53d7824626cc4b8e139f96
123rhkddms2 <123rhkddms2@gmail.com>
2023-12-29 16:50:55 +0900

    exam수정

--- Exam/src/test2/Test08.java
+++ Exam/src/test2/Test08.java
@@ -7,7 +7,7 @@ package test2;
  */
 public class Test08 {
 	public static void main(String[] args) {
-		
+			
 		System.out.println("3! = "  +factorial(3) );
 		System.out.println("4! = "  +factorial(4) );
 		System.out.println("5! = "  +factorial(5) );
--- Exam/src/test2/Test10.java
+++ Exam/src/test2/Test10.java
@@ -9,7 +9,9 @@ public class Test10 {
 	public static void main(String[] args) {
 		
 		for(int i=0; i<10; i++) {
-			System.out.print(fibo(i) + " ");
+			
+			int result = fibo(i);
+			System.out.print(result + " ");
 		}
 	}
 	public static int fibo(int n) {
@@ -17,6 +19,6 @@ public class Test10 {
 			return n;
 		}
 		
-		return fibo(n-2) + fibo(n-1);
+		return fibo(n-1) + fibo(n-2);
 	}
 }
