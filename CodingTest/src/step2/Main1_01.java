0cba14d6001f473d3ca9eac7b8002521390afe5c
123rhkddms2 <123rhkddms2@gmail.com>
2023-12-29 17:46:20 +0900

    코딩테스트 수정

--- CodingTest/src/step2/Main1_01.java
+++ CodingTest/src/step2/Main1_01.java
@@ -6,6 +6,18 @@ public class Main1_01 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a>b) {
+			System.out.println(">");
+		}
+		else if(a<b){
+			System.out.println("<");
+		}
+		else {
+			System.out.println("==");
+		}
 	
 	}
 
