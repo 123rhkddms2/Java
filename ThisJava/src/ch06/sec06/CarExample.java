f18663e1489c124e774d7ee9fb1b45460d9f1be6
123rhkddms2 <123rhkddms2@gmail.com>
2024-01-02 14:01:33 +0900

    thisjava 수정

---
+++ ThisJava/src/ch06/sec06/CarExample.java
@@ -0,0 +1,14 @@
+package ch06.sec06;
+
+public class CarExample {
+	public static void main(String[] args) {
+		
+		//Car 객체 생성
+		Car s = new Car();
+		
+		//Car 객체의 필드값 읽기
+		System.out.println("모델명 : " + s.model);
+		System.out.println("시동여부 : " + s.start);
+		System.out.println("현재속도 : " + s.speed);
+	}
+}
